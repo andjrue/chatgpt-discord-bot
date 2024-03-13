7e33b014e8d7195b7a4042222890e508e074738c
andjrue <drewsimcox12@gmail.com>
2024-03-13 17:27:47 -0400

    Making sure gitignore works

--- src/main.go
+++ src/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	disKey = os.GetEnv("DISCORD_KEY")
+
+	discord, err := discord.go("Bot " + disKey)
+	if err != nil {
+		fmt.Println("Error creating session")
+	}
+}
