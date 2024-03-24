1dd98f02b08cd3133a5fbad93a3c6009200ea544
andjrue <drewsimcox12@gmail.com>
2024-03-24 10:32:52 -0400

    Need to rethink how this is going to work

--- main.go
+++ main.go
@@ -11,21 +11,24 @@ import (
 
 * Message Sending: DONE
 	Need the bot to send a message when someone joins a chat channel
+	Update on this, totally unnecessary. See the readme.
 
 * Bot needs to join a voice channel whenever someone joins one: DONE
-	Will still need to send the message as well
+	Will still need to send the message as well - No it won't.
 
 * Small issue with above, the bot never actually leaves the channel.
 	Need to find a way to check how many members are in the channel at any given time.
 	When it's <= 1, the bot can leave/turn off?
 
 * Need to connect to OpenAI API:
-	The idea is that when someone joins a voice channel, the bot follows and will text-to-speech
-	the "Someone Joined" message
+	Not happening
 
 * Maybe deploy this to an EC2 instance?
 	Not 100% on this yet. Ideally, this won't run from my computer. I do plan on using it.
 	Just not sure if that's the best way to do it, but it would be a fun AWS project.
+
+*** I'm going to need to rethink a lot of this. Encoding and Decoding audio is a lot more difficult than I thought it
+	it would be. I thought I was 90% done, I'm probably 30% done. Sigh.
 
 */
 
@@ -40,6 +43,9 @@ func userHasJoinedVoice(s *discordgo.Session, v *discordgo.VoiceStateUpdate) {
 	// fmt.Println("Function call is working") DEBUG
 
 	if v.BeforeUpdate == nil {
+		if v.UserID == s.State.User.ID { // Bot was sending messages when they joined
+			return
+		}
 		fmt.Print("User has joined voice channel")
 		user, err := s.User(v.VoiceState.UserID)
 		if err != nil {
@@ -70,7 +76,6 @@ func botJoinChannel(s *discordgo.Session, v *discordgo.VoiceStateUpdate) {
 			fmt.Println("Error joining voice channel", err)
 		}
 	}
-
 }
 
 func main() {
@@ -99,82 +104,10 @@ func main() {
 
 	fmt.Println("Bot is online")
 
-	/*
-	                             __xxxxxxxxxxxxxxxx___.
-	                        _gxXXXXXXXXXXXXXXXXXXXXXXXX!x_
-	                   __x!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!x_
-	                ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx_
-	              ,gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!_
-	            _!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
-	          gXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXs
-	        ,!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!.
-	       g!XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	      iXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	     ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
-	     !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
-	   ,XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
-	   !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXi
-	  dXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	  !XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-	   XXXXXXXXXXXXXXXXXXXf~~~VXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvvvvXXXXXXXXXXXXXX!
-	   !XXXXXXXXXXXXXXXf`       'XXXXXXXXXXXXXXXXXXXXXf`          '~XXXXXXXXXXP
-	    vXXXXXXXXXXXX!            !XXXXXXXXXXXXXXXXXX!              !XXXXXXXXX
-	     XXXXXXXXXXv`              'VXXXXXXXXXXXXXXX                !XXXXXXXX!
-	     !XXXXXXXXX.                 YXXXXXXXXXXXXX!                XXXXXXXXX
-	      XXXXXXXXX!                 ,XXXXXXXXXXXXXX                VXXXXXXX!
-	      'XXXXXXXX!                ,!XXXX ~~XXXXXXX               iXXXXXX~
-	       'XXXXXXXX               ,XXXXXX   XXXXXXXX!             xXXXXXX!
-	        !XXXXXXX!xxxxxxs______xXXXXXXX   'YXXXXXX!          ,xXXXXXXXX
-	         YXXXXXXXXXXXXXXXXXXXXXXXXXXX`    VXXXXXXX!s. __gxx!XXXXXXXXXP
-	          XXXXXXXXXXXXXXXXXXXXXXXXXX!      'XXXXXXXXXXXXXXXXXXXXXXXXX!
-	          XXXXXXXXXXXXXXXXXXXXXXXXXP        'YXXXXXXXXXXXXXXXXXXXXXXX!
-	          XXXXXXXXXXXXXXXXXXXXXXXX!     i    !XXXXXXXXXXXXXXXXXXXXXXXX
-	          XXXXXXXXXXXXXXXXXXXXXXXX!     XX   !XXXXXXXXXXXXXXXXXXXXXXXX
-	          XXXXXXXXXXXXXXXXXXXXXXXXx_   iXX_,_dXXXXXXXXXXXXXXXXXXXXXXXX
-	          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXP
-	          XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	           ~vXvvvvXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
-	                    'VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXvvvvvv~
-	                      'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX~
-	                  _    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
-	                 -XX!  !XXXXXXX~XXXXXXXXXXXXXXXXXXXXXX~   Xxi
-	                  YXX  '~ XXXXX XXXXXXXXXXXXXXXXXXXX`     iXX`
-	                  !XX!    !XXX` XXXXXXXXXXXXXXXXXXXX      !XX
-	                  !XXX    '~Vf  YXXXXXXXXXXXXXP YXXX     !XXX
-	                  !XXX  ,_      !XXP YXXXfXXXX!  XXX     XXXV
-	                  !XXX !XX           'XXP 'YXX!       ,.!XXX!
-	                  !XXXi!XP  XX.                  ,_  !XXXXXX!
-	                  iXXXx X!  XX! !Xx.  ,.     xs.,XXi !XXXXXXf
-	                   XXXXXXXXXXXXXXXXX! _!XXx  dXXXXXXX.iXXXXXX
-	                   VXXXXXXXXXXXXXXXXXXXXXXXxxXXXXXXXXXXXXXXX!
-	                   YXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXV
-	                    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX!
-	                    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
-	                       VXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXf
-	                         VXXXXXXXXXXXXXXXXXXXXXXXXXXXXv`
-	                          ~vXXXXXXXXXXXXXXXXXXXXXXXf`
-	                              ~vXXXXXXXXXXXXXXXXv~
-	                                 '~VvXXXXXXXV~~
-	                                       ~~
-	*/
-
 	select {} // DO NOT REMOVE THIS
 
 	/*
-	* After hours of googling, someone suggested this on a random stackoverflow post.
-	* The bot was automatically shutting off before any voice updates could be detected.
-	* Adding the select statement fixed the issue, messages are now sending
-	* when someone joins a call.
-	 */
+		The select statement prevents the bot from turning off.
+		Thank you random person on stackoverflow.
+	*/
 }
